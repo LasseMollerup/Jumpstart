4ebde0a54e03020ec0198a13404b98f69004246a
Lasse Mollerup <l.mollerup@hotmail.com>
2019-07-04 14:01:12 -0500

    tried to get the slick slider to work on the Carousel. However I am doing something wrong

---
+++ resources/java/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#slideshow .slick').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 2000,
+        fade: true
+    });
+  });
