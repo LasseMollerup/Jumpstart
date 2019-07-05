e1c872c81c9fe286b89ac9d2c985d2b0b48e64f5
LasseMollerup <l.mollerup@hotmail.com>
2019-07-04 22:39:01 -0500

    I have gotten the slick carousel to work and have styled most of it. However I am still missing a bit css to get it perfect.

--- resources/java/main.js
+++ resources/java/main.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-    $('#slideshow .slick').slick({
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        speed: 2000,
-        fade: true
+    $('#slideshow').slick({
+      autoplay: true,
+      autospeed: 2000,
+      speed: 2000,
+      fade: true,
     });
   });
